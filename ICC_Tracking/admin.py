60b483c17f00173018a9dad4002b99502f66db94
Kieranator5050 <thejugg63@gmail.com>
2021-11-24 20:36:42 -0500

    Created Models

--- ICC_Tracking/admin.py
+++ ICC_Tracking/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db.models import fields
 
 # Register your models here.
+from .models import Customer, Order, Bearer
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('ID','orderID','fname','lname','company','address','optionalAddressInfo','city','country','contactNumber','contactEmail')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('orderID','assignedCustomer','assignedBearer','subtotal','orderStatus')
+
+class BearerAdmin(admin.ModelAdmin):
+    list_display = ('ID','name','assignedOrders','location')
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Bearer, BearerAdmin)
--- ICC_Tracking/models.py
+++ ICC_Tracking/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    ID = models.AutoField(primary_key=True)
+    orderID = models.CharField(max_length=30, blank=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    company = models.CharField(max_length=50, default="N/A")
+    address = models.CharField(max_length=50)
+    optionalAddressInfo = models.CharField(max_length=50, default="N/A")
+    city = models.CharField(max_length=50, default="Portmore")
+    country = models.CharField(max_length=50, default="Jamaica")
+    contactNumber = models.CharField(max_length=50, blank=True)
+    contactEmail = models.EmailField(max_length=50, blank=True)
+
+class Order(models.Model):
+
+    STATUS_CHOICES = (
+       (1, "Preparing"),
+       (2, "Active"),
+        (3, "Delivered"),
+    )
+
+    orderID = models.CharField(max_length=50, primary_key=True)
+    assignedCustomer = models.IntegerField()
+    assignedBearer = models.IntegerField(blank=True)
+    subtotal = models.FloatField(blank=True)
+    orderStatus = models.IntegerField(choices=STATUS_CHOICES, default=1)
+
+class Bearer(models.Model):
+    ID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    assignedOrders = models.CharField(max_length=50, blank=True)
+    location = models.CharField(max_length=50, blank=True)
