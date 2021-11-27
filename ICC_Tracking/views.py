2327b9d2e3f42aff742c5e2fbdcaf8e3ee4258b6
J.Wan <jaysun1w@gmail.com>
2021-11-27 17:57:08 -0500

    Email feedback added

--- ICC_Tracking/views.py
+++ ICC_Tracking/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.core.mail import send_mail
 
 # Create your views here.
 # request -> response
@@ -12,7 +13,25 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 #Landing Page
 def index(request):
-    return render(request, 'index.html')
+
+    # Checks if there is a post request on the webpage, and collects the name of the person,email and feedback they have written
+    if request.method == "POST":
+        message_name =  request.POST['realname']
+        message_email = request.POST['email']
+        message = request.POST['comments']
+
+        #Sends email with feedback to the owner/business
+        send_mail( 'Feedback from '+ message_name,message + '\n Customer email: ' + message_email, message_email,['icecc.feedback@gmail.com'])
+        #Sends confirmation email to the customer
+        send_mail('Feedback received', 'Thank you for the feedback','icecc.feedback@gmail.com', [message_email])
+
+        return render(request,'index.html',{'message_name':message_name})
+    else:
+        return render(request, 'index.html')
+
+
+
+
 
 #Inventory Page
 @staff_member_required()
@@ -23,3 +42,4 @@ def inventory(request):
 @staff_member_required()
 def admin(request):
     return render(request, 'admin.html')
+
