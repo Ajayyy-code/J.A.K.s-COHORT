a4eb4253ae1e0ffb227cd364e7b77dc61709274d
Kieranator5050 <thejugg63@gmail.com>
2021-11-30 18:12:15 -0500

    Allowing all hosts to connect

--- ICC_Sys/settings.py
+++ ICC_Sys/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a&hir3+bp9vq#gkh_78dpw0l^t%9hr5%1meka8@bvt=v1jl9x9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
